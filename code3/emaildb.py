fec114654c896afd068c953d1b69a5265ee34abb
Paduck <hjyu1408@gmail.com>
2019-06-26 10:33:43 -0400

    june 26 study

--- code3/emaildb.py
+++ code3/emaildb.py
@@ -1,10 +1,14 @@
+#import Library
 import sqlite3
 
+#connect to Library
 conn = sqlite3.connect('emaildb.sqlite')
 cur = conn.cursor()
 
+#if there is existing table,delete it / in case of none table, this code do notihg
 cur.execute('DROP TABLE IF EXISTS Counts')
 
+#Create New table
 cur.execute('''
 CREATE TABLE Counts (email TEXT, count INTEGER)''')
 
@@ -15,7 +19,11 @@ for line in fh:
     if not line.startswith('From: '): continue
     pieces = line.split()
     email = pieces[1]
+
+# ? 는 sql에서 자리표기, 뒤의 것에 의해 대체
+#Not read/put data, just make cusor() ready
     cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,))
+#Grab first thing
     row = cur.fetchone()
     if row is None:
         cur.execute('''INSERT INTO Counts (email, count)
---
+++ ex_14_2/ex_14_2.py
@@ -0,0 +1,5 @@
+class Fourcal :
+    def setdata(self,first,second) :
+        self.first = first
+        self.second = second
+        #a.setdata(4,2)
